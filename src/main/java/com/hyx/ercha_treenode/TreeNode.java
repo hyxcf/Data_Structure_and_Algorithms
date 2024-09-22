2c221bf7657a85304469354f41e1d86ed03327f8
hyx <3259679405@qq.com>
2024-09-22 17:59:57 +0800

    树节点类

---
+++ src/main/java/com/hyx/ercha_treenode/TreeNode.java
@@ -0,0 +1,32 @@
+package com.hyx.ercha_treenode;
+
+/**
+ * @Author：hyx
+ * @Date：2024/9/22 17:41
+ * 树节点类
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
