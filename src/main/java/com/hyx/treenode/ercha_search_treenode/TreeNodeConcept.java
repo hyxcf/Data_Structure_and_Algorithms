4979def3c2ebcfbfad824efce38c44c161cc697f
hyx <3259679405@qq.com>
2024-09-24 21:59:15 +0800

    二叉搜索树的概念

---
+++ src/main/java/com/hyx/treenode/ercha_search_treenode/TreeNodeConcept.java
@@ -0,0 +1,15 @@
+package com.hyx.treenode.ercha_search_treenode;
+
+/**
+ * @Author：hyx
+ * @Date：2024/9/24 14:59
+ */
+public class TreeNodeConcept {
+
+    /*
+        二叉搜索树
+            1.树节点增加key属性，用来比较谁大谁小，key不可以重复
+            2.对于任意一个树节点，它的key比左子树的key都大，同时也比右子树的key都小
+     */
+
+}
