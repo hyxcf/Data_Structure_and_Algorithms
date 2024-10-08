eed4c76c83f6e85b090443f5c0fa2fe0d45f26e5
hyx <3259679405@qq.com>
2024-10-08 22:59:25 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/hyx/leetcode/hash/E02LeetCode3.java
@@ -0,0 +1,22 @@
+package com.hyx.leetcode.hash;
+
+/**
+ * @version 0.1
+ * @Author hyx
+ * @className E02LeetCode3
+ * @Date 2024/10/8  22:42
+ * @description 无重复字符的最长子串
+ * @since jdk 11
+ */
+public class E02LeetCode3 {
+
+    /**
+     * 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+     * 输入: s = "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     */
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+}
