58c9ac5d78862ac714406a14aafb497c1d6faf6f
hyx <3259679405@qq.com>
2024-10-13 15:00:57 +0800

    桶排序

---
+++ src/main/java/com/hyx/sort/BucketSort.java
@@ -0,0 +1,27 @@
+package com.hyx.sort;
+
+import java.util.Arrays;
+
+/**
+ * @version 0.1
+ * @Author hyx
+ * @className BucketSort
+ * @Date 2024/10/13  14:57
+ * @description 桶排序
+ * @since jdk 11
+ */
+public class BucketSort {
+
+    public static void main(String[] args) {
+        int[] ages = {20,18,28,66,25,31,67,30};// 假设人类年龄 99
+        System.out.println(Arrays.toString(ages));
+        sort(ages);
+        System.out.println(Arrays.toString(ages));
+
+    }
+
+    private static void sort(int[] a) {
+
+    }
+
+}
