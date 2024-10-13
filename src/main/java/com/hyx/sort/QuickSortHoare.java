cd850be484f4feea11d58cd5a2430d527353e86e
hyx <3259679405@qq.com>
2024-10-13 15:00:32 +0800

    快排-双边循环

--- src/main/java/com/hyx/sort/QuickSortHoare.java
+++ src/main/java/com/hyx/sort/QuickSortHoare.java
@@ -1,6 +1,7 @@
 package com.hyx.sort;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @version 0.1
@@ -33,8 +34,26 @@ public class QuickSortHoare {
           2.2 j 从右向左
          3、最后基准点与 i 交换，i 即为基准点最终索引
      */
+    /*
+        注意事项：
+            1.为啥要加内层循环的 i < j 条件
+            2.为啥要先处理 j，再处理 i
+            3.随机元素作为基准点
+            4.数组中如果有大量重复元素
+     */
+    /*
+        4.的解决办法要点（看 QuickSortHandleDuplicate 类）
+            循环内
+                i 从 left + 1 开始，从左向右找大的或相等的
+                j 从 right 开始，从右向左找小的或相等的
+                交换，i++ j--
+            循环外 j 和 基准点交换，j 即为分区位置
+     */
     private static int partition(int[] a, int left, int right) {
+        int idx = ThreadLocalRandom.current().nextInt(right - left + 1) + left;
+        // [0~9] right - left + 1 = 3    [0,2] + 4 =[4,6]
         // 1、选择最左侧元素作为基准点
+        swap(a,idx,left);
         int pv = a[left];
         int i = left; // i 从左往右 找大的
         int j = right;// j 从右往左 找小的
