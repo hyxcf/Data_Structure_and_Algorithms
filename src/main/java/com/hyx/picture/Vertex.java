8b45477c6d980c7145201961152e3bd15dd1969b
hyx <3259679405@qq.com>
2024-10-17 19:28:52 +0800

    顶点

---
+++ src/main/java/com/hyx/picture/Vertex.java
@@ -0,0 +1,56 @@
+package com.hyx.picture;
+
+import java.util.List;
+
+/**
+ * @version 0.1
+ * @Author hyx
+ * @className Vertex
+ * @Date 2024/10/17  14:43
+ * @description 顶点
+ * @since jdk 11
+ */
+public class Vertex {
+
+    String name;
+    List<Edge> edges;
+    boolean visited; // 是否被访问过,用在 BFS 和 DFS
+    int inDegree; // 入度，用在拓扑排序
+    int status; // 状态 0-未访问 1-访问中 2-访问过，用在拓扑排序
+    int dist = INF; // 用于 迪克斯特拉 算法表示距离
+    static final Integer INF = Integer.MAX_VALUE;
+    Vertex prev = null;
+
+
+    public Vertex(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+        Vertex a = new Vertex("A");
+        Vertex b = new Vertex("B");
+        Vertex c = new Vertex("C");
+        Vertex d = new Vertex("D");
+
+        a.edges = List.of(new Edge(b), new Edge(c));
+        b.edges = List.of(new Edge(d));
+        c.edges = List.of(new Edge(d));
+        d.edges = List.of();
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "name='" + name + '\'' +
+                ", dist=" + dist +
+
+
+                '}';
+    }
+}
+
