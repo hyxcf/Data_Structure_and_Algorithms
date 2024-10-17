59b70cd45fa506a5d50b99712e00fef621421aba
hyx <3259679405@qq.com>
2024-10-17 19:27:33 +0800

    深度优先搜索

---
+++ src/main/java/com/hyx/picture/Dfs.java
@@ -0,0 +1,59 @@
+package com.hyx.picture;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @version 0.1
+ * @Author hyx
+ * @className Vertex
+ * @Date 2024/10/17  14:43
+ * @description 深度优先搜索
+ * @since jdk 11
+ */
+
+public class Dfs {
+    public static void main(String[] args) {
+        Vertex v1 = new Vertex("v1");
+        Vertex v2 = new Vertex("v2");
+        Vertex v3 = new Vertex("v3");
+        Vertex v4 = new Vertex("v4");
+        Vertex v5 = new Vertex("v5");
+        Vertex v6 = new Vertex("v6");
+
+        v1.edges = List.of(new Edge(v3), new Edge(v2), new Edge(v6));
+        v2.edges = List.of(new Edge(v4));
+        v3.edges = List.of(new Edge(v4), new Edge(v6));
+        v4.edges = List.of(new Edge(v5));
+        v5.edges = List.of();
+        v6.edges = List.of(new Edge(v5));
+
+        dfs2(v1);
+    }
+
+    private static void dfs2(Vertex v) {
+        LinkedList<Vertex> stack = new LinkedList<>();
+        stack.push(v);
+        while (!stack.isEmpty()) {
+            Vertex pop = stack.pop();
+            pop.visited = true;
+            System.out.println(pop.name);
+            // 遍历弹出顶点是否有相邻的边
+            for (Edge edge : pop.edges) {
+                if (!edge.linked.visited) {
+                    stack.push(edge.linked);
+                }
+            }
+        }
+    }
+
+    private static void dfs1(Vertex v) {
+        v.visited = true;
+        System.out.println(v.name);
+        for (Edge edge : v.edges) {
+            if (!edge.linked.visited) {
+                dfs1(edge.linked);
+            }
+        }
+    }
+}
