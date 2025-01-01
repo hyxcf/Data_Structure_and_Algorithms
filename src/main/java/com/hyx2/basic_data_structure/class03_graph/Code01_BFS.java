458f20e7d91d68932850bc2d92f1ee0146fe1992
hyx <3259679405@qq.com>
2025-01-01 23:19:37 +0800

    图的宽度优先遍历

---
+++ src/main/java/com/hyx2/basic_data_structure/class03_graph/Code01_BFS.java
@@ -0,0 +1,38 @@
+package com.hyx2.basic_data_structure.class03_graph;
+
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author hyx
+ * @version 0.1
+ * @className Code01_BFS
+ * @date 2025/1/1 22:15
+ * @description
+ * @since jdk 11
+ */
+public class Code01_BFS {
+
+    // 从 node 出发，进行宽度优先遍历
+    public static void bfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> set = new HashSet<>();
+        queue.add(node);
+        set.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    queue.add(cur);
+                    set.add(cur);
+                }
+            }
+        }
+    }
+
+}
