50f9a347f33b9eed854db6fe3b61287ddb727ed4
hyx <3259679405@qq.com>
2025-01-06 15:47:36 +0800

    暴力递归：汉诺塔问题(终于理解了)

---
+++ src/main/java/com/hyx2/basic_data_structure/class04_recur/Code01_Hanoi.java
@@ -0,0 +1,33 @@
+package com.hyx2.basic_data_structure.class04_recur;
+
+/**
+ * @author hyx
+ * @version 0.1
+ * @className Code01_Hanoi
+ * @date 2025/1/6 10:58
+ * @description
+ * @since jdk 11
+ */
+public class Code01_Hanoi {
+
+    public static void main(String[] args) {
+        hanoi(4);
+    }
+
+    public static void hanoi(int n) {
+        if (n > 0) {
+            process(n, "left", "mid", "right");
+        }
+    }
+
+    private static void process(int n, String from, String to, String other) {
+        if (n == 1) {
+            System.out.println("Move 1 from " + from + " to " + to);
+        }else {
+            process(n - 1, from, other, to);
+            System.out.println("Move " + n + " from " + from + " to " + to);
+            process(n - 1, other, to, from);
+        }
+    }
+
+}
