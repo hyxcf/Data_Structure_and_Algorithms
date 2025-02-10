2dd5279e7834f5aa08202fc67b9e1b8abdfb6136
hyx <3259679405@qq.com>
2025-02-11 00:24:13 +0800

    动态规划问题 - 记忆化搜索 - 背包问题的动态规划版本

--- src/main/java/com/hyx2/basic_data_structure/class05_dynamic_plan/Code02_Knapsack_improve.java
+++ src/main/java/com/hyx2/basic_data_structure/class05_dynamic_plan/Code02_Knapsack_improve.java
@@ -31,4 +31,11 @@ public class Code02_Knapsack_improve {
         return dp[0][bag];
     }
 
+    public static void main(String[] args) {
+        int[] weights = {3, 2, 4, 7};
+        int[] values = {5, 6, 3, 19};
+        int bag = 11;
+        System.out.println(dpway(weights, values, bag));
+    }
+
 }
