9e196df5aa0c44cc54a92459ab2938d2babcead9
hyx <3259679405@qq.com>
2025-08-22 09:04:09 +0800

    链表：回文链表 方法2

--- src/main/java/com/zuochengyun/class02ListNode/IsPalindromeNodes.java
+++ src/main/java/com/zuochengyun/class02ListNode/IsPalindromeNodes.java
@@ -9,7 +9,30 @@ public class IsPalindromeNodes {
 
     // 方法2
     // 将方法1的步骤1和步骤2进行合并，在一次循环里既找到中间点，也把前半个链表进行反转。然后进行步骤3 ，比较两个链表
-
+    public static boolean isPalindrome2(ListNode head){
+        ListNode p1 = head; //慢指针
+        ListNode p2 = head; //快指针
+        ListNode n1 = null;
+        ListNode o1 = head;
+        while (p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+            o1.next = n1;
+            n1 = o1;
+            o1 = p1;
+        }                                                                                        
+        if (p2 != null){
+            p1= p1.next;
+        }
+        while(n1 != null){
+            if(n1.val != p1.val){
+                return false;
+            }
+            n1 = n1.next;
+            p1 = p1.next;
+        }
+        return true;
+    }
 
     // 方法1：先找到中间节点，然后再反转半个链表，逐一进行比较
     public static boolean isPalindrome(ListNode p1) {
