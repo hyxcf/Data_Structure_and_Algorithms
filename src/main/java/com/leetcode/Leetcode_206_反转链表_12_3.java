511a6af44f56adf2614e5f8ebd95d86eda595dc6
hyx <3259679405@qq.com>
2025-12-04 09:26:04 +0800

    力扣：206_反转链表

---
+++ src/main/java/com/leetcode/Leetcode_206_反转链表_12_3.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import com.hyx.leetcode.listnode.ListNode;
+
+import java.util.List;
+
+public class Leetcode_206_反转链表_12_3 {
+
+    // 方法1：新增一个链表
+    public ListNode reverseListNode1(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode p = null;
+        while (node != null) {
+            ListNode tempNestListNode = node.next;
+            node.next = p;
+            p = node;
+            node = tempNestListNode;
+        }
+        return p;
+    }
+
+
+    // 方法2：递归 尾插法
+    public ListNode reverseListNode2(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode newHead = reverseListNode2(node.next);
+        node.next.next = node;
+        node.next = null;
+        return newHead;
+    }
+    
+    // 方法4：头插法
+    public ListNode reverseListNode4(ListNode node){
+        if (node == null || node.next == null) return node;
+        ListNode n1 = null;
+        while (node != null){
+            ListNode o2 = node.next;
+            node.next = n1;
+            n1 = node;
+            node = o2;
+        }
+        return n1;
+    }
+
+}
