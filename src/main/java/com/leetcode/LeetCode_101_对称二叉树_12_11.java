00dd509b07fa306c1d0a74594a093095bc16b871
hyx <3259679405@qq.com>
2025-12-11 09:51:54 +0800

    力扣：101_对称二叉树

---
+++ src/main/java/com/leetcode/LeetCode_101_对称二叉树_12_11.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+public class LeetCode_101_对称二叉树_12_11 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+
+}
