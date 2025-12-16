68a558027a802706ebca04a3f09440a32d1f7e8b
hyx <3259679405@qq.com>
2025-12-16 15:17:28 +0800

    力扣：98:验证二叉搜索树

---
+++ src/main/java/com/leetcode/LeetCode_98_验证二叉搜索树_12_16.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import java.util.Stack;
+
+public class LeetCode_98_验证二叉搜索树_12_16 {
+
+    // 中序遍历验证是否是二叉搜索树，我只需要知道中序遍历，在遍历时 检查每一个新节点是否比前一个大 即可。
+    public boolean isValidBST(TreeNode root) {
+        TreeNode p = root;
+        Stack<TreeNode> stack = new Stack<>();
+        long prev = Long.MIN_VALUE;
+        while (p != null || !stack.isEmpty()) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode pop = stack.pop();
+                if (prev >= pop.val) {
+                    return false;
+                }
+                prev = pop.val;
+                p = pop.right;
+            }
+        }
+        return true;
+    }
+
+    long prev = Long.MIN_VALUE;
+
+    public boolean isValidBST2(TreeNode root) {
+        if (root == null) return true;
+        boolean a = isValidBST(root.left);
+        if (!a) return false;
+        if (prev > root.val) {
+            return false;
+        }
+        prev = root.val;
+        return isValidBST(root.right);
+    }
+
+}
+/*                  [10]
+                prev : 8
+        10      3 5 7 8 10
+       /
+      5
+     / \
+    3   7
+         \
+          8
+ */
