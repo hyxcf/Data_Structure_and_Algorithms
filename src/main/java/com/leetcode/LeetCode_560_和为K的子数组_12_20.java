6e7369fbcc9ca259a1a166025f39c2b8a888fc34
hyx <3259679405@qq.com>
2025-12-20 10:14:00 +0800

    力扣：560_和为K的子数组

---
+++ src/main/java/com/leetcode/LeetCode_560_和为K的子数组_12_20.java
@@ -0,0 +1,56 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_560_和为K的子数组_12_20 {
+
+    public int subarraySum(int[] nums, int k) {
+        return 0;
+    }
+
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            // 前缀和
+            // 数组元素   0 1 1 1
+            // 前缀和     0 1 2 3
+            // 出现的次数 1 1 1 1
+            // K=2 当 前缀和-k=前缀和的任意一项 res + 1
+            // 使用map存储前缀和 和 它出现的次数
+            int pre = 0;
+            int count = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            for (int i = 0; i < nums.length; i++) {
+                pre += nums[i];
+                // 满足条件的前缀和
+                if (map.containsKey(pre - k)) {
+                    count += map.get(pre - k);
+                }
+                if (map.containsKey(pre)) {
+                    map.put(pre, map.get(pre) + 1);
+                } else {
+                    map.put(pre, 1);
+                }
+            }
+            return count;
+        }
+    }
+
+    class Solution2 {
+        public int subarraySum(int[] nums, int k) {
+            Map<Integer, Integer> cnt = new HashMap<>(nums.length + 1, 1); // 预分配空间
+            cnt.put(0, 1); // s[0]=0 单独统计
+            int s = 0;
+            int ans = 0;
+            for (int x : nums) {
+                s += x;
+                ans += cnt.getOrDefault(s - k, 0);
+                cnt.merge(s, 1, Integer::sum); // cnt[s]++
+            }
+            return ans;
+        }
+    }
+
+}
+
