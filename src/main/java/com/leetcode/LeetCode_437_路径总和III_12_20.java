23ef1f1a117ba12aa3db584b3e6d4d1f49c7537a
hyx <3259679405@qq.com>
2025-12-22 19:34:29 +0800

    力扣：437_路径总和III

---
+++ src/main/java/com/leetcode/LeetCode_437_路径总和III_12_20.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_437_路径总和III_12_20 {
+
+    private int ans;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        // 和 560 题一样套路，使用前缀和
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        dfs(root, 0, targetSum, map);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int s, int targetSum, Map<Integer, Integer> map) {
+        if (node == null) {
+            return;
+        }
+        s += node.val;
+        ans += map.getOrDefault(s - targetSum, 0);
+        map.merge(s, 1, Integer::sum); // map[s]++
+        dfs(node.left, s, targetSum, map);
+        dfs(node.right, s, targetSum, map);
+        map.merge(s, -1, Integer::sum); // map[s]-- 恢复现场（撤销 cnt[s]++）
+    }
+
+}
