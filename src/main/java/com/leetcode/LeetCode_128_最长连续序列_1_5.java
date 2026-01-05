85c575738f39246bbd54a97c40ff30d723ea6d97
hyx <3259679405@qq.com>
2026-01-05 09:47:33 +0800

    哈希专题复习：49,128不熟

---
+++ src/main/java/com/leetcode/LeetCode_128_最长连续序列_1_5.java
@@ -0,0 +1,46 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LeetCode_128_最长连续序列_1_5 {
+    
+/*
+    给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+    请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+    
+    示例 1：
+        输入：nums = [100,4,200,1,3,2]
+        输出：4
+    解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+    
+    示例 2：
+        输入：nums = [0,3,7,2,5,8,4,6,0,1]
+        输出：9
+    
+    示例 3：
+        输入：nums = [1,0,1,2]
+        输出：3    
+ */
+
+    public int longestConsecutive(int[] nums) {
+        int res = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        for (Integer x : set) {
+            if (set.contains(x - 1)) {
+                continue;
+            }
+            int y = x + 1;
+            while (set.contains(y)) {
+                y++;
+            }
+            res = Math.max(res, y - x);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/leetcode/Leetcode_49_字母异位词分组_1_5.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Leetcode_49_字母异位词分组_1_5 {
+
+    /*
+        给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+    
+        示例 1:
+            输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+            输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        解释：
+            在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+            字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+            字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            char[] temp = new char[26];
+            for (char c : chars) {
+                temp[c - 'a']++;
+            }
+            map.computeIfAbsent(String.valueOf(temp), v -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
