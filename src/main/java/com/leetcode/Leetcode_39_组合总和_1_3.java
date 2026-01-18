94bb2c547f00983e8fdba19bc4d9141ad65c0b34
hyx <3259679405@qq.com>
2026-01-18 16:23:14 +0800

    回溯窗口专题复习:39

--- src/main/java/com/leetcode/Leetcode_39_组合总和_1_3.java
+++ src/main/java/com/leetcode/Leetcode_39_组合总和_1_3.java
@@ -1,5 +1,9 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author hyx
  * @version 0.1
@@ -54,5 +58,50 @@ public class Leetcode_39_组合总和_1_3 {
     │
     └─ i=3, choose 7 → state=[7], target=0 → ✅ 加入 res: [[2,2,3], [7]]
 */
+
+    /*
+        给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+        candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+        对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+        示例 1：
+            输入：candidates = [2,3,6,7], target = 7
+            输出：[[2,2,3],[7]]
+            解释：
+            2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+            7 也是一个候选， 7 = 7 。
+            仅有这两种组合。
+        示例 2：
+            输入: candidates = [2,3,5], target = 8
+            输出: [[2,2,2,2],[2,3,3],[3,5]]
+        示例 3：
+            输入: candidates = [2], target = 1
+            输出: []
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates.length == 0) {
+            return res;
+        }
+        Arrays.sort(candidates);
+        List<Integer> temp = new ArrayList<>();
+        backtracking(0, target, candidates, temp, res);
+        return res;
+    }
+
+    private void backtracking(int index, int target, int[] candidates, List<Integer> temp, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            int choice = candidates[i];
+            if (target - choice < 0) {
+                break; // 剪枝
+            }
+            temp.add(choice);
+            backtracking(i, target - choice, candidates, temp, res); // fixme:这里的值是可以重复的，因此这里传入的是i
+            temp.remove(temp.size() - 1);
+        }
+    }
 
 }
