58107676bd45c8cbd620b8cfc38af9899129ff49
hyx <3259679405@qq.com>
2026-01-25 00:20:20 +0800

    力扣：4_寻找两个正序数组的中位数

--- src/main/java/com/leetcode/LeetCode_4_寻找两个正序数组的中位数_1_23.java
+++ src/main/java/com/leetcode/LeetCode_4_寻找两个正序数组的中位数_1_23.java
@@ -16,8 +16,52 @@ public class LeetCode_4_寻找两个正序数组的中位数_1_23 {
             解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
      */
 
+    /*
+
+     */
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0;
+        int m = nums1.length;
+        int n = nums2.length;
+        // 第一步：m < n
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        // 第二步：初始化i的区间
+        int iMin = 0;
+        int iMax = m;
+        // 第三步：循环判断
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (i > 0 && j < n && nums1[i - 1] > nums2[j]) { // 缩小右区间
+                iMax = i - 1;
+            } else if (j > 0 && i < m && nums2[j - 1] > nums1[i]) { // 增大左区间
+                iMin = i + 1;
+            } else {
+                // i 刚好不大也不小，则求出左右部分的最值
+                int maxLeft = 0;
+                int minRight = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                // 如果是奇数，则直接返回左边的最大值
+                if ((m + n) % 2 == 1) return maxLeft;
+                if (j == n) {
+                    minRight = nums1[i];
+                } else if (i == m) {
+                    minRight = nums2[j];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+                return (minRight + maxLeft) / 2.0;
+            }
+        }
+        return 0d;
     }
 
 }
