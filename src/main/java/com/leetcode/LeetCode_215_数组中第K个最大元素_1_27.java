ef8e2aa72a8e562632e112f47490767ebabe08b3
hyx <3259679405@qq.com>
2026-01-27 12:32:51 +0800

    力扣：215_数组中第K个最大元素

--- src/main/java/com/leetcode/LeetCode_215_数组中第K个最大元素_1_27.java
+++ src/main/java/com/leetcode/LeetCode_215_数组中第K个最大元素_1_27.java
@@ -77,3 +77,20 @@ public class LeetCode_215_数组中第K个最大元素_1_27 {
         }
     }
 }
+/*
+    情况 2：if (nums.size() - small.size() < k)
+    先简化这个条件：
+    nums.size() - small.size() = big.size() + equal.size()
+    所以条件等价于：如果 k > big.size() + equal.size()
+    意思是：第 k 大 不在 big 也不在 equal 里，只能在 small 里。
+    那在 small 中要找第几个大呢？
+    原来整个数组有 n = nums.size() 个元素。
+    small 里有 s = small.size() 个元素，它们是 最小的 s 个，也就是排名从 (n - s + 1) 到 n。
+    所以，如果我们要找第 k 大，而它在 small 中，那么它在 small 内部的“第几大”是多少？
+    答案是：k - (n - s)
+    因为前 (n - s) 个最大值（即 big + equal）已经被排除了。
+    而 n - s = big.size() + equal.size()，所以：
+    在 small 中找第 k - (big.size() + equal.size()) 大
+    但 big.size() + equal.size() = n - small.size()
+    所以就是 k - (n - small.size()) = k - n + small.size()
+ */
