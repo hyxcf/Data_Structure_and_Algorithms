0a5d0a6c72433c1c5833ff797e8fba9cf9650871
hyx <3259679405@qq.com>
2026-02-01 16:04:31 +0800

    力扣(复习)：回溯：79：单词搜索

--- src/main/java/com/leetcode/Leetcode_79_单词搜索_1_21.java
+++ src/main/java/com/leetcode/Leetcode_79_单词搜索_1_21.java
@@ -50,5 +50,44 @@ public class Leetcode_79_单词搜索_1_21 {
             return false;
         }
     }
-    
+
+    static class Preview_2_1 {
+        public boolean exist(char[][] board, String word) {
+            if (board == null || board.length == 0 || board[0].length == 0) {
+                return false;
+            }
+            int m = board.length;
+            int n = board[0].length;
+            boolean[][] visited = new boolean[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (backtrack(0, i, j, board, word, visited)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean backtrack(int index, int i, int j, char[][] board, String word, boolean[][] visited) {
+            if (index == word.length()) {
+                return true;
+            }
+            if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(index) != board[i][j]) {
+                return false;
+            }
+            visited[i][j] = true;
+            if (backtrack(index + 1, i - 1, j, board, word, visited)
+                    || backtrack(index + 1, i + 1, j, board, word, visited)
+                    || backtrack(index + 1, i, j + 1, board, word, visited)
+                    || backtrack(index + 1, i, j - 1, board, word, visited)) {
+                return true;
+            } else {
+                visited[i][j] = false;
+                return false;
+            }
+        }
+    }
+
+
 }
