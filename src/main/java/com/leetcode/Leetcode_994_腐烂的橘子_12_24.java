357d72cc66609ad813f8ac674c36c46c6866f863
hyx <3259679405@qq.com>
2026-02-02 11:25:48 +0800

    力扣(复习)：图论：994：腐烂的橘子

--- src/main/java/com/leetcode/Leetcode_994_腐烂的橘子_12_24.java
+++ src/main/java/com/leetcode/Leetcode_994_腐烂的橘子_12_24.java
@@ -2,6 +2,7 @@ package com.leetcode;
 
 import java.util.LinkedList;
 import java.util.Queue;
+
 /*
 这道题的本质就是 bfs 求最短路径
  */
@@ -58,6 +59,77 @@ public class Leetcode_994_腐烂的橘子_12_24 {
             return round;
         }
     }
+
+    /*
+        在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
+        值 0 代表空单元格；
+        值 1 代表新鲜橘子；
+        值 2 代表腐烂的橘子。
+        每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
+        返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
+        示例 1：
+            输入：grid = [[2,1,1],[1,1,0],[0,1,1]]
+            输出：4
+        示例 2：
+            输入：grid = [[2,1,1],[0,1,1],[1,0,1]]
+            输出：-1
+            解释：左下角的橘子（第 2 行， 第 0 列）永远不会腐烂，因为腐烂只会发生在 4 个方向上。
+     */
+    static class Preview_2_2 {
+        public int orangesRotting(int[][] grid) {
+            // 求最短路径
+            int m = grid.length;
+            int n = grid[0].length;
+            int count = 0;
+            Queue<int[]> queue = new LinkedList<>();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
+                        count++;
+                    } else if (grid[i][j] == 2) {
+                        queue.offer(new int[]{i, j});
+                    }
+                }
+            }
+            int round = 0;
+            while (count > 0 && !queue.isEmpty()) {
+                round++;
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    int[] bad = queue.poll();
+                    int row = bad[0];
+                    int col = bad[1];
+                    if (row - 1 >= 0 && grid[row - 1][col] == 1) {
+                        grid[row - 1][col] = 2;
+                        count--;
+                        queue.offer(new int[]{row - 1, col});
+                    }
+                    if (row + 1 < m && grid[row + 1][col] == 1) {
+                        grid[row + 1][col] = 2;
+                        count--;
+                        queue.offer(new int[]{row + 1, col});
+                    }
+                    if (col - 1 >= 0 && grid[row][col - 1] == 1) {
+                        grid[row][col - 1] = 2;
+                        count--;
+                        queue.offer(new int[]{row, col - 1});
+                    }
+                    if (col + 1 < n && grid[row][col + 1] == 1) {
+                        grid[row][col + 1] = 2;
+                        count--;
+                        queue.offer(new int[]{row, col + 1});
+                    }
+                }
+            }
+            // fixme:有新鲜的橘子没有被污染的时候，则返回-1
+            if (count > 0) {
+                return -1;
+            } else {
+                return round;
+            }
+        }
+    }
+
 }
 
 /*
