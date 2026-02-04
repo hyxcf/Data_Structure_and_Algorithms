10bb6ab83a382467a301cbc718e94cbfbe9ff78a
hyx <3259679405@qq.com>
2026-02-04 20:55:02 +0800

    力扣(复习)：二叉树：108：将有序数组转化为二叉搜索树（复习什么是平衡搜索二叉树）

--- src/main/java/com/leetcode/LeetCode_108_将有序数组转化为二叉搜索树_12_16.java
+++ src/main/java/com/leetcode/LeetCode_108_将有序数组转化为二叉搜索树_12_16.java
@@ -19,4 +19,31 @@ public class LeetCode_108_将有序数组转化为二叉搜索树_12_16 {
         return root;
     }
 
+    /*
+     平衡二叉搜索树的核心特点：
+        1、平衡：左右子树的高度差不超过1
+        2、二叉搜索树：
+            BST 的性质（对任意节点 node）：
+            左子树中所有节点的值 < node.val
+            右子树中所有节点的值 > node.val
+            左右子树也都是 BST
+     */
+    static class Preview_2_4 {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return dfs2(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode dfs2(int[] nums, int begin, int last) {
+            if (begin > last) {
+                return null;
+            }
+            int mid = begin + (last - begin) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = dfs2(nums, begin, mid - 1);
+            root.right = dfs2(nums, mid + 1, last);
+            return root;
+        }
+
+    }
+
 }
