290ebdfed16185c6d5df416e2d2c5ec4ef075b3a
hyx <3259679405@qq.com>
2026-02-07 16:52:59 +0800

    力扣(复习)：链表：142：环形链表2

--- src/main/java/com/leetcode/Leetcode_142_环形链表2_12_4.java
+++ src/main/java/com/leetcode/Leetcode_142_环形链表2_12_4.java
@@ -6,6 +6,7 @@ public class Leetcode_142_环形链表2_12_4 {
 
     /**
      * 判断链表中是否有环，如果有环找到入环的第一个节点
+     *
      * @param head
      * @return
      */
@@ -17,8 +18,8 @@ public class Leetcode_142_环形链表2_12_4 {
             f = f.next.next;
             if (f == l) {
                 f = head;
-                while (true){
-                    if (l == f){
+                while (true) {
+                    if (l == f) {
                         return l;
                     }
                     f = f.next;
@@ -28,5 +29,29 @@ public class Leetcode_142_环形链表2_12_4 {
         }
         return null;
     }
-    
+
+    private static class Preview_2_7 {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null) return null;
+
+            ListNode slow = head, fast = head;
+
+            // 第一步：找相遇点
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    // 第二步：找入口
+                    fast = head;
+                    while (slow != fast) {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+    }
+
 }
