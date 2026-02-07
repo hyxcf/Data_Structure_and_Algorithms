43203890e222f920982bf3b4a6e90d34dc7d829b
hyx <3259679405@qq.com>
2026-02-07 15:56:38 +0800

    力扣(复习)：链表：206：反转链表

--- src/main/java/com/leetcode/Leetcode_206_反转链表_12_3.java
+++ src/main/java/com/leetcode/Leetcode_206_反转链表_12_3.java
@@ -32,18 +32,62 @@ public class Leetcode_206_反转链表_12_3 {
         node.next = null;
         return newHead;
     }
-    
+
     // 方法4：头插法
-    public ListNode reverseListNode4(ListNode node){
+    public ListNode reverseListNode4(ListNode node) {
         if (node == null || node.next == null) return node;
         ListNode n1 = null;
-        while (node != null){
+        while (node != null) {
             ListNode o2 = node.next;
             node.next = n1;
             n1 = node;
             node = o2;
         }
         return n1;
+    }
+
+    static class Preview_2_7 {
+        // 三种方法
+        // 1.新增一个链表p
+        public ListNode reverseListNode1(ListNode node) {
+            if (node == null || node.next == null) {
+                return node;
+            }
+            ListNode p = null;
+            while (node != null) {
+                ListNode tempNode = node.next;
+                node.next = p;
+                p = node;
+                node = tempNode;
+            }
+            return p;
+        }
+
+        // 2、尾插法 递归
+        public ListNode reverseListNode2(ListNode node) {
+            if (node == null || node.next == null) {
+                return node;
+            }
+            ListNode newHead = reverseListNode2(node.next);
+            node.next.next = node;
+            node.next = null;
+            return newHead;
+        }
+
+        // 3、头插法
+        public ListNode reverseListNode3(ListNode node) {
+            if (node == null || node.next == null) {
+                return node;
+            }
+            ListNode n1 = null;
+            while (node != null) {
+                ListNode o2 = node.next;
+                node.next = n1;
+                n1 = node;
+                node = o2;
+            }
+            return n1;
+        }
     }
 
 }
