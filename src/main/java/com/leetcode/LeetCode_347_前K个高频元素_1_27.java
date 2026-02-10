b36f1611a7fef7255c9171b9387ee05ed15bfb30
hyx <3259679405@qq.com>
2026-02-10 21:02:16 +0800

    力扣(复习)：堆：347：前K个高频元素

--- src/main/java/com/leetcode/LeetCode_347_前K个高频元素_1_27.java
+++ src/main/java/com/leetcode/LeetCode_347_前K个高频元素_1_27.java
@@ -3,7 +3,7 @@ package com.leetcode;
 import java.util.*;
 
 public class LeetCode_347_前K个高频元素_1_27 {
-    
+
     /*
         给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
         示例 1：
@@ -18,20 +18,20 @@ public class LeetCode_347_前K个高频元素_1_27 {
      */
     // 最小堆
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num,map.getOrDefault(num,0) +1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (minHeap.size() < k){
+            if (minHeap.size() < k) {
                 minHeap.offer(entry);
-            } else if (entry.getValue() > minHeap.peek().getValue()){
+            } else if (entry.getValue() > minHeap.peek().getValue()) {
                 minHeap.poll();
                 minHeap.offer(entry);
             }
         }
-        int [] res = new int [k];
+        int[] res = new int[k];
         for (int i = k - 1; i >= 0; i--) {
             res[i] = minHeap.poll().getKey();
         }
@@ -41,9 +41,9 @@ public class LeetCode_347_前K个高频元素_1_27 {
 
     // 排序法
     public int[] topKFrequent2(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num,map.getOrDefault(num,0) +1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         // Step 2: 转为列表并按频率降序排序
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
@@ -55,5 +55,52 @@ public class LeetCode_347_前K个高频元素_1_27 {
             res[i] = list.get(i).getKey();
         }
         return res;
+    }
+
+    private static class Preview_2_10 {
+        // 最小堆
+        public int[] topKFrequent(int[] nums, int k) {
+            if (nums.length == 0) {
+                return nums;
+            }
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (minHeap.size() < k) {
+                    minHeap.offer(entry);
+                } else {
+                    if (entry.getValue() > minHeap.peek().getValue()) {
+                        minHeap.poll();
+                        minHeap.offer(entry);
+                    }
+                }
+            }
+            int[] res = new int[k];
+            for (int i = k - 1; i >= 0; i--) {
+                res[i] = minHeap.poll().getKey();
+            }
+            return res;
+        }
+
+        // 排序法
+        public int[] topKFrequent2(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            // Step 2: 转为列表并按频率降序排序
+            List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+            list.sort((a, b) -> b.getValue() - a.getValue()); // 高频在前
+
+            // Step 3: 取前 k 个 key（每个 key 是唯一的，不会重复）
+            int[] res = new int[k];
+            for (int i = 0; i < k; i++) {
+                res[i] = list.get(i).getKey();
+            }
+            return res;
+        }
     }
 }
