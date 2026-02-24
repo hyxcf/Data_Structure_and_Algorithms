2b9c07754830d6b89f90d6b05cf9b14dad3b9ae1
hyx <3259679405@qq.com>
2026-02-24 20:22:05 +0800

    力扣(复习)：二叉树：108：将有序数组转化为二叉搜索树(思想着重复习)

--- src/main/java/com/leetcode/LeetCode_108_将有序数组转化为二叉搜索树_12_16.java
+++ src/main/java/com/leetcode/LeetCode_108_将有序数组转化为二叉搜索树_12_16.java
@@ -1,7 +1,5 @@
 package com.leetcode;
 
-import java.util.Stack;
-
 public class LeetCode_108_将有序数组转化为二叉搜索树_12_16 {
 
     public TreeNode sortedArrayToBST(int[] nums) {
@@ -44,6 +42,23 @@ public class LeetCode_108_将有序数组转化为二叉搜索树_12_16 {
             return root;
         }
 
+    }
+
+    private static class Preview_2_24 {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return dfs(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode dfs(int[] nums, int begin, int last) {
+            if (begin > last) {
+                return null;
+            }
+            int mid = begin + (last - begin) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = dfs(nums, begin, mid - 1);
+            root.right = dfs(nums, mid + 1, last);
+            return root;
+        }
     }
 
 }
