973c02786f0392f405658fefa6c73f9f74594343
hyx <3259679405@qq.com>
2026-02-24 20:38:54 +0800

    力扣(复习)：二叉树：199：二叉树的右视图（两种方法）

--- src/main/java/com/leetcode/Leetcode_199_二叉树的右视图_12_17.java
+++ src/main/java/com/leetcode/Leetcode_199_二叉树的右视图_12_17.java
@@ -98,4 +98,47 @@ public class Leetcode_199_二叉树的右视图_12_17 {
         }
     }
 
+    private static class Preview_2_24 {
+        // 二叉树的右视图 1、深度优先遍历 2、宽度优先遍历
+
+        // 深度优先遍历
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            dfs(root, 1, res);
+            return res;
+        }
+
+        private void dfs(TreeNode node, int depth, List<Integer> res) {
+            if (node == null) {
+                return;
+            }
+            if (res.size() < depth) {
+                res.add(node.val);
+            }
+            dfs(node.right, depth + 1, res);
+            dfs(node.left, depth + 1, res);
+        }
+
+        // 宽度优先遍历，层序遍历
+        public List<Integer> rightSideView2(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            List<Integer> res = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    root = queue.poll();
+                    if (i == size - 1) res.add(root.val);
+                    if (root.left != null) queue.offer(root.left);
+                    if (root.right != null) queue.offer(root.right);
+                }
+            }
+            return res;
+        }
+
+    }
+
 }
