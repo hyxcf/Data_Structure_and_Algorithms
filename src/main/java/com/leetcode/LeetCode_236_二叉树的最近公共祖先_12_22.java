e16c0b8177c6419de94735cc66ea09a635437cac
hyx <3259679405@qq.com>
2026-02-25 17:42:45 +0800

    力扣(复习)：二叉树：236：二叉树的最近公共祖先

--- src/main/java/com/leetcode/LeetCode_236_二叉树的最近公共祖先_12_22.java
+++ src/main/java/com/leetcode/LeetCode_236_二叉树的最近公共祖先_12_22.java
@@ -63,6 +63,27 @@ public class LeetCode_236_二叉树的最近公共祖先_12_22 {
         }
     }
 
+    // 二叉树的最近公共祖先
+    private static class Preview_2_25 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left == null && right == null) {
+                return null;
+            }
+            if (left == null) {
+                return right;
+            }
+            if (right == null) {
+                return left;
+            }
+            return root;
+        }
+    }
+
 }
 
 /*
