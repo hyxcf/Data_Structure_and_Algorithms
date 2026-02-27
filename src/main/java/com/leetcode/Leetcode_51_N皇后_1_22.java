53b7a614abdd0b5e9d9d990b4d7ccadc783cfcf9
hyx <3259679405@qq.com>
2026-02-27 20:32:08 +0800

    力扣(复习)：回溯：51：N皇后（着重复习）

--- src/main/java/com/leetcode/Leetcode_51_N皇后_1_22.java
+++ src/main/java/com/leetcode/Leetcode_51_N皇后_1_22.java
@@ -125,4 +125,75 @@ public class Leetcode_51_N皇后_1_22 {
         }
     }
 
+    private static class Preview_2_27 {
+        /*
+            按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+            n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+            给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+            每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+            示例 1：
+                输入：n = 4
+                输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+            解释：如上图所示，4 皇后问题存在两个不同的解法。
+            示例 2：
+                输入：n = 1
+                输出：[["Q"]]
+
+            行、列、对角线（主对角线、副对角线）都不能用多余的皇后
+            如何判断 主副对角线 是否有皇后
+            [
+            [0,0][0,1][0,2][0,3]
+            [1,0][1,1][1,2][1,3]
+            [2,0][2,1][2,2][2,3]
+            [3,0][3,1][3,2][3,3]
+            ]
+            主对角线：i-j 都是相等的 最大值 3 最小值 -3 所需空间2n-1
+            副对角线：i+j 都是相等的 最大值 3 最小值 -3 所需空间2n-1
+         */
+        public List<List<String>> solveNQueens(int n) {
+            List<List<String>> res = new ArrayList<>();
+            if (n == 0) {
+                return res;
+            }
+            char[][] board = new char[n][n];
+            for (char[] chars : board) {
+                Arrays.fill(chars, '.');
+            }
+            boolean[] cols = new boolean[n];
+            boolean[] dig1 = new boolean[2 * n - 1];
+            boolean[] dig2 = new boolean[2 * n - 1];
+            backtracking(0, n, cols, dig1, dig2, board, res);
+            return res;
+        }
+
+        private void backtracking(int row, int n, boolean[] cols, boolean[] dig1, boolean[] dig2, char[][] board, List<List<String>> res) {
+            if (row == n){
+                List<String> temp = new ArrayList<>();
+                for (char[] r : board) {
+                    temp.add(String.valueOf(r));
+                }
+                res.add(temp);
+                return;
+            }
+            for (int col = 0; col < n; col++){
+                // 主对角线
+                int d1 = row - col + n - 1;
+                // 副对角线
+                int d2 = row + col;
+                if (cols[col] || dig1[d1] || dig2[d2]) {
+                    continue;
+                }
+                cols[col] = true;
+                dig1[d1] = true;
+                dig2[d2] = true;
+                board[row][col] = 'Q';
+                backtracking(row + 1, n, cols, dig1, dig2, board, res);
+                board[row][col] = '.';
+                cols[col] = false;
+                dig1[d1] = false;
+                dig2[d2] = false;
+            }
+        }
+    }
+
 }
