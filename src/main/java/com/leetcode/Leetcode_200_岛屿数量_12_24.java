c5ccf6305c4b822b5d4ef6c4bf4d4a1629728e0b
hyx <3259679405@qq.com>
2026-03-11 11:59:11 +0800

    力扣(复习)：图论

--- src/main/java/com/leetcode/Leetcode_200_岛屿数量_12_24.java
+++ src/main/java/com/leetcode/Leetcode_200_岛屿数量_12_24.java
@@ -111,5 +111,29 @@ public class Leetcode_200_岛屿数量_12_24 {
         }
     }
 
+    private static class Preview_3_10 {
+        // 岛屿数量
+        public int numIslands(char[][] grid) {
+            if (grid.length == 0 || grid[0].length == 0) return 0;
+            int res = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '1') {
+                        dfs(i, j, grid);
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
 
+        private void dfs(int i, int j, char[][] grid) {
+            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+            grid[i][j] = '0';
+            dfs(i - 1, j, grid);
+            dfs(i + 1, j, grid);
+            dfs(i, j - 1, grid);
+            dfs(i, j + 1, grid);
+        }
+    }
 }
