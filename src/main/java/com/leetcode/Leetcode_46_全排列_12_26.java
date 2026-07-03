46ac52430f07c7a59530e6db6333729cd9d96997
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-03 17:18:10 +0800

    力扣(复习)：Leetcode_46_全排列

--- src/main/java/com/leetcode/Leetcode_46_全排列_12_26.java
+++ src/main/java/com/leetcode/Leetcode_46_全排列_12_26.java
@@ -291,4 +291,50 @@ dfs(0) @ [1,2,3]
         }
     }
 
+    /*
+    给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+    示例 1：
+        输入：nums = [1,2,3]
+        输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    示例 2：
+        输入：nums = [0,1]
+        输出：[[0,1],[1,0]]
+    示例 3：
+        输入：nums = [1]
+        输出：[[1]]
+        fixme: 总结：不是“第一个数不动”，而是“在处理第 index 位时，前面 index 个数已经固定，不再参与交换；当前层只在 [index, end] 范围内交换，选一个放到 index 位置，然后递归处理 index+1”。
+*/
+    private static class Preview_7_3 {
+        public List<List<Integer>> permute(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return new ArrayList<>();
+            }
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> path = new ArrayList<>();
+            for (int num : nums) {
+                path.add(num);
+            }
+            backtrack(0, res, path);
+            return res;
+        }
+
+        private void backtrack(int index, List<List<Integer>> res, List<Integer> path) {
+            if (index == path.size()) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = index; i < path.size(); i++) {
+                swap(path, i, index);
+                backtrack(index + 1, res, path);
+                swap(path, i, index);
+            }
+        }
+
+        private void swap(List<Integer> arr, int i, int j) {
+            int temp = arr.get(i);
+            arr.set(i, arr.get(j));
+            arr.set(j, temp);
+        }
+    }
+
 }
