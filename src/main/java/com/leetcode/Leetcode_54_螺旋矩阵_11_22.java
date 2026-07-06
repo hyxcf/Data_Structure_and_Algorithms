b34ad0d16f36b8d7e0383b954a629e24e9ce9b09
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-06 10:53:14 +0800

    力扣(复习)：Leetcode_54_螺旋矩阵

--- src/main/java/com/leetcode/Leetcode_54_螺旋矩阵_11_22.java
+++ src/main/java/com/leetcode/Leetcode_54_螺旋矩阵_11_22.java
@@ -96,4 +96,49 @@ public class Leetcode_54_螺旋矩阵_11_22 {
         }
     }
 
+    private static class Preview_7_6 {
+        /*
+            给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+            示例 1：
+                输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+                输出：[1,2,3,6,9,8,7,4,5]
+            示例 2：
+                输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+                输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+         */
+        public List<Integer> spiralOrder(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return new ArrayList<>();
+            }
+            int left = 0;
+            int right = matrix[0].length - 1;
+            int top = 0;
+            int bottom = matrix.length - 1;
+            List<Integer> res = new ArrayList<>();
+            while (true) {
+                // 从左到右
+                for (int i = left; i <= right; i++) {
+                    res.add(matrix[top][i]);
+                }
+                if (++top > bottom) break;
+                // 从上到下
+                for (int i = top; i <= bottom; i++) {
+                    res.add(matrix[i][right]);
+                }
+                if (--right < left) break;
+                // 从右到左
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                if (--bottom < top) break;
+                // 从下到上
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                if (++left > right) break;
+            }
+            return res;
+        }
+    }
+
 }
