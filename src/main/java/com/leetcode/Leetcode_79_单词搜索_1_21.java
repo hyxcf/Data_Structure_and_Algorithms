24be8526e50559ecfd357bc21bb9268e20de420c
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-13 10:57:47 +0800

    力扣(复习)：Leetcode_79_单词搜索

--- src/main/java/com/leetcode/Leetcode_79_单词搜索_1_21.java
+++ src/main/java/com/leetcode/Leetcode_79_单词搜索_1_21.java
@@ -273,5 +273,54 @@ public class Leetcode_79_单词搜索_1_21 {
         }
     }
 
+    private static class Preview_7_13 {
+        /*
+            给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+            单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+            示例 1：
+                输入：board = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], word = "ABCCED"
+                输出：true
+            示例 2：
+                输入：board = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], word = "SEE"
+                输出：true
+                fixme: 行和列注意进行区分
+         */
+        public boolean exist(char[][] board, String word) {
+            if (board == null || board[0].length == 0) {
+                return false;
+            }
+            int row = board.length;
+            int col = board[0].length;
+            boolean[][] visited = new boolean[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (backtack(0, i, j, board, visited, word)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean backtack(int index, int i, int j, char[][] board, boolean[][] visited, String word) {
+            if (index == word.length()) {
+                return true;
+            }
+            if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || word.charAt(index) != board[i][j]) {
+                return false;
+            }
+            visited[i][j] = true;
+            if (backtack(index + 1, i + 1, j, board, visited, word)
+                    || backtack(index + 1, i - 1, j, board, visited, word)
+                    || backtack(index + 1, i, j + 1, board, visited, word)
+                    || backtack(index + 1, i, j - 1, board, visited, word)
+            ) {
+                return true;
+            } else {
+                visited[i][j] = false;
+                return false;
+            }
+        }
+    }
 
 }
