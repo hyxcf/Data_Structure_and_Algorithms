a8fc1a48cc62d74477656d260f8eb840e2d70171
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-13 09:50:50 +0800

    力扣(复习)：Leetcode_73_矩阵置零

--- src/main/java/com/leetcode/Leetcode_73_矩阵置零_11_21.java
+++ src/main/java/com/leetcode/Leetcode_73_矩阵置零_11_21.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import org.checkerframework.checker.units.qual.A;
+
 /**
  * 11_21 十一月二十一号
  * 73：力扣第73题
@@ -118,18 +120,18 @@ public class Leetcode_73_矩阵置零_11_21 {
             int col = matrix[0].length;
             boolean flagCol = false, flagRow = false;
             for (int i = 0; i < row; i++) {
-                if (matrix[i][0] == 0){
+                if (matrix[i][0] == 0) {
                     flagCol = true;
                 }
             }
             for (int i = 0; i < col; i++) {
-                if (matrix[0][i] == 0){
+                if (matrix[0][i] == 0) {
                     flagRow = true;
                 }
             }
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
-                    if (matrix[i][j] == 0){
+                    if (matrix[i][j] == 0) {
                         matrix[i][0] = 0;
                         matrix[0][j] = 0;
                     }
@@ -137,17 +139,96 @@ public class Leetcode_73_矩阵置零_11_21 {
             }
             for (int i = 1; i < row; i++) {
                 for (int j = 1; j < col; j++) {
-                    if (matrix[i][0] == 0 || matrix[0][j] == 0){
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                         matrix[i][j] = 0;
                     }
                 }
             }
-            if (flagCol){
+            if (flagCol) {
                 for (int i = 0; i < row; i++) {
                     matrix[i][0] = 0;
                 }
             }
-            if (flagRow){
+            if (flagRow) {
+                for (int i = 0; i < col; i++) {
+                    matrix[0][i] = 0;
+                }
+            }
+        }
+
+    }
+
+    private static class Preview_7_13 {
+        /*
+        给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+        示例 1：
+            输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+            输出：[[1,0,1],[0,0,0],[1,0,1]]
+        示例 2：
+            输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+            输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+         */
+        public void setZeros(int[][] matrix) {
+            int row = matrix.length;
+            int col = matrix[0].length;
+            boolean[] r = new boolean[row];
+            boolean[] l = new boolean[col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (matrix[i][j] == 0) {
+                        r[i] = true;
+                        l[j] = true;
+                    }
+                }
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (r[i] || l[j]) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // O(1)的空间复杂度 使用第一排和第一列进行标记
+        public void setZero2(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return;
+            }
+            int row = matrix.length;
+            int col = matrix[0].length;
+            boolean firstCol = false, firstRow = false;
+            for (int i = 0; i < row; i++) {
+                if (matrix[i][0] == 0) {
+                    firstCol = true;
+                }
+            }
+            for (int i = 0; i < col; i++) {
+                if (matrix[0][i] == 0) {
+                    firstRow = true;
+                }
+            }
+            for (int i = 1; i < row; i++) {
+                for (int j = 1; j < col; j++) {
+                    if (matrix[i][j] == 0) {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+            for (int i = 1; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            if (firstCol) {
+                for (int i = 0; i < row; i++) {
+                    matrix[i][0] = 0;
+                }
+            }
+            if (firstRow) {
                 for (int i = 0; i < col; i++) {
                     matrix[0][i] = 0;
                 }
