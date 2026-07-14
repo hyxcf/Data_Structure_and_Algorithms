d5d3bf6e53f1c75b1ab2c69a2db2179b0a2704ae
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-14 20:09:52 +0800

    力扣(复习)：Leetcode_90_子集II

--- src/main/java/com/leetcode/Leetcode_90_子集II_1_18.java
+++ src/main/java/com/leetcode/Leetcode_90_子集II_1_18.java
@@ -133,4 +133,40 @@ public class Leetcode_90_子集II_1_18 {
 
     }
 
+    private static class Preview_7_14 {
+        /*
+            给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的 子集（幂集）。
+            解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+            示例 1：
+                输入：nums = [1,2,2]
+                输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+            示例 2：
+                输入：nums = [0]
+                输出：[[],[0]]
+         */
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (nums == null || nums.length == 0) {
+                return res;
+            }
+            // fixme:这道题记得排序
+            Arrays.sort(nums);
+            List<Integer> path = new ArrayList<>();
+            backtrack(0, nums, path, res);
+            return res;
+        }
+
+        private void backtrack(int index, int[] nums, List<Integer> path, List<List<Integer>> res) {
+            res.add(new ArrayList<>(path));
+            for (int i = index; i < nums.length; i++) {
+                if (i != index && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                path.add(nums[i]);
+                backtrack(i + 1, nums, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
 }
