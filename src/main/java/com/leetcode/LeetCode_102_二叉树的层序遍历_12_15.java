d01cbaf217f465022d7d35dd325a4610b79e3895
hyxcf2 <67903582+hyxcf2@users.noreply.github.com>
2026-07-22 10:41:08 +0800

    力扣(复习)：LeetCode_102_二叉树的层序遍历

--- src/main/java/com/leetcode/LeetCode_102_二叉树的层序遍历_12_15.java
+++ src/main/java/com/leetcode/LeetCode_102_二叉树的层序遍历_12_15.java
@@ -152,4 +152,31 @@ public class LeetCode_102_二叉树的层序遍历_12_15 {
         }
     }
 
+    private static class Preview_7_22 {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            List<List<Integer>> res = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> tempList = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode poll = queue.poll();
+                    tempList.add(poll.val);
+                    if (poll.left != null) {
+                        queue.offer(poll.left);
+                    }
+                    if (poll.right != null) {
+                        queue.offer(poll.right);
+                    }
+                }
+                res.add(tempList);
+            }
+            return res;
+        }
+    }
+
 }
